c58a4371f0d806ff8f3246d8288108e9ad301a7e
iloveicedgreentea <31193909+iloveicedgreentea@users.noreply.github.com>
2023-05-14 12:35:42 -0400

    cancel tasks

--- custom_components/madvr/remote.py
+++ custom_components/madvr/remote.py
@@ -72,14 +72,25 @@ class MadvrCls(RemoteEntity):
         self.mac = mac
         self.attrs = {}
         self.hass = hass
+        self.tasks = []
+        self.connection_event = self.madvr_client.connection_event
 
         self.command_queue = asyncio.Queue()
 
     async def async_added_to_hass(self) -> None:
         """Run when entity about to be added to hass."""
-        self.hass.loop.create_task(self.madvr_client.open_connection())
-        self.hass.loop.create_task(self.handle_queue())
-        self.hass.loop.create_task(self.madvr_client.read_notifications())
+        task = self.hass.loop.create_task(self.madvr_client.open_connection())
+        self.tasks.append(task)
+        task = self.hass.loop.create_task(self.handle_queue())
+        self.tasks.append(task)
+        task = self.hass.loop.create_task(self.madvr_client.read_notifications())
+        self.tasks.append(task)
+    
+    async def async_will_remove_from_hass(self) -> None:
+        self.madvr_client.stop()
+        for task in self.tasks:
+            if not task.done():
+                task.cancel()
 
     @property
     def should_poll(self):
@@ -117,6 +128,7 @@ class MadvrCls(RemoteEntity):
     async def handle_queue(self):
         """Handle items in command queue."""
         while True:
+            await self.connection_event.wait()
             # send all commands in queue
             while not self.command_queue.empty():
                 command = await self.command_queue.get()
